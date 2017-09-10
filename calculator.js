8f2bdfac9205801a359a74ab4e2b85999518dd73
billcarrollxc <31629324+billcarrollxc@users.noreply.github.com>
2017-09-10 14:24:28 -0400

    debug

--- calculator.js
+++ calculator.js
@@ -1202,7 +1202,7 @@ function totalTime() {
 
 function pacePercentages() {
 	var trainingSec = (60*document.pacePercentages.trainingPaceMin) + document.pacePercentages.trainingPaceSec;
-	var raceSec = (60*document.pacePercentaes.racePaceMin) + document.pacePercentages.racePaceSec;
+	var raceSec = (60*document.pacePercentages.racePaceMin) + document.pacePercentages.racePaceSec;
 	var percent = trainingSec/raceSec;
 	
 	if (percent > 1) {
